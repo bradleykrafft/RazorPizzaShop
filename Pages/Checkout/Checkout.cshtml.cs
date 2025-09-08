b72f746d2bb64df55cc9fb763f2355c8e2240acd
bradleykrafft <bradleykrafft@outlook.com>
2025-09-08 15:58:04 -0400

    Setting up Pizza Page

---
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzaShop.Pages.Checkout
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
