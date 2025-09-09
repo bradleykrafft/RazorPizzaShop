0b9537d357f3209ad6d05e39cc7dd1d7c1922112
bradleykrafft <bradleykrafft@outlook.com>
2025-09-09 15:44:36 -0400

    Updating UI and adding Pizza Model

---
+++ Models/PizzasModel.cs
@@ -0,0 +1,22 @@
+﻿namespace RazorPizzaShop.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTitle { get; set; }
+        public string PiazzaName { get; set; }
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce {  get; set; }
+        public bool WhiteSauce {  get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+        public bool Ham { get; set; }
+        public bool Pinapple { get; set; }
+        public bool ItalianSausage { get; set; }
+        public bool Mushroom { get; set; }
+        public bool Beef { get; set; }
+        public bool Tuna { get; set; }
+
+        public float FinalPrice { get; set; }
+
+    }
+}
