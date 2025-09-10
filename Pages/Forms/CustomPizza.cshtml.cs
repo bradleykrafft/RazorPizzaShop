9d2e01db9ab9b45b2967c65ba9789d1c4078245b
bradleykrafft <bradleykrafft@outlook.com>
2025-09-10 19:33:42 -0400

    Checkout and ThankYou pages edited.

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -8,9 +8,29 @@ namespace RazorPizzaShop.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
-
+        public float PizzaPrice { get; set; } 
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.WhiteSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if(Pizza.ItalianSausage) PizzaPrice += 1;
+            if (Pizza.Pinapple) PizzaPrice += 10;
+            if (Pizza.Beef) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new {Pizza.PiazzaName, PizzaPrice});
+
+        }
+
     }
 }
